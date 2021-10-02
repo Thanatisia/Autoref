66e58b10a2be92b1ebd50f6015621087a3b3dde0
Thanatisia <55834101+Thanatisia@users.noreply.github.com>
2021-10-02 18:35:11 +0800

    Updated

--- main/workspace/main.py
+++ main/workspace/main.py
@@ -1,8 +1,13 @@
 import os
 import sys
+import tkinter as tk
+import sqlite3 as db
 
 def main():
 	print("Hello World")
-
+	root = tk.Tk()
+	root.mainloop()
+	
 if __name__ == "__main__":
 	main()
+
